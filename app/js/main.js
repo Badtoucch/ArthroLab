e31e5fa00e8876957b6e84d653d7683b131e2006
Vladislad T <v.trundenko@everad.com>
2019-06-26 13:08:43 +0300

    vlad

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	var $menu = $("#menu");
+	$(window).scroll(function(){
+			if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
+					$menu.removeClass("default").addClass("fixed");
+			} else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+					$menu.removeClass("fixed").addClass("default");
+			}
+	});
+	$("#toggle-mnu").on('click', function(e) {
+		e.preventDefault();
+		$(this).toggleClass("on");
+		$(this).parent().toggleClass('navbar_hide');
+	});
+});
